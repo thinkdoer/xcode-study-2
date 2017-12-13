45354c230f7b5146e789a6a88b888fd23cb54d46
thinkdoer <rich-hun@hotmail.com>
2017-12-13 12:59:38 +0800

    Initial Commit

---
+++ xcode-study-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  xcode-study-2
+//
+//  Created by Richard on 13/12/2017.
+//  Copyright © 2017 Richard. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
